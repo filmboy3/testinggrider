b385efddd93c155e2dd67084e8c150069715b485
filmboy3 <jonathanschwartz30@gmail.com>
2019-02-03 13:29:44 -0500

    first pretty

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
   render() {
-                            return (
-                  <div className="App">
-              Hi there!
-        </div>
-    );
+    return <div className="App">Hi there!</div>;
   }
 }
 
