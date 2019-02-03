f5dbbdb42e2d34b3776e7cff81dccfd6c8faa6ad
filmboy3 <jonathanschwartz30@gmail.com>
2019-02-03 15:03:09 -0500

    completed user input test with force re-render

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -18,3 +18,13 @@ it("has a text area and a button", () => {
   expect(wrapped.find("textarea").length).toEqual(1);
   expect(wrapped.find("button").length).toEqual(1);
 });
+
+it("has a text area that users can type in", () => {
+  wrapped.find("textarea").simulate("change", {
+    target: { value: "new comment" }
+  });
+  // Force re-render
+  wrapped.update();
+
+  expect(wrapped.find("textarea").prop("value")).toEqual("new comment");
+});
