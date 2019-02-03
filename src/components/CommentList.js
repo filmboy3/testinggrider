3beaec4e72d882c8495e3f210a14b1b8a9715eb0
filmboy3 <jonathanschwartz30@gmail.com>
2019-02-03 14:36:30 -0500

    added text box button handlers

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,13 +1,28 @@
 import React, { Component } from "react";
 
 class CommentBox extends Component {
+  state = { comment: "" };
+
+  handleChange = event => {
+    this.setState({ comment: event.target.value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    // TODO - Call an action creator
+    // And save the comment
+
+    this.setState({ comment: "" });
+  };
+
   render() {
     return (
       <form>
         <h4>Add a Comment</h4>
-        <textarea />
+        <textarea onChange={this.handleChange} value={this.state.comment} />
         <div>
-          <button>Submit Comment</button>
+          <button onSubmit={this.handleSubmit}>Submit Comment</button>
         </div>
       </form>
     );
