0253bbf071707a0a0a20459a74ddab8b1eed53b0
filmboy3 <jonathanschwartz30@gmail.com>
2019-02-03 13:11:59 -0500

    added react-redux

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        Hi there!
+              <div className="App">
+              Hi there!
       </div>
     );
   }
