19a6f77aaa94683ee61024a89dac7f4177eba46e
filmboy3 <jonathanschwartz30@gmail.com>
2019-02-03 13:59:11 -0500

    completed first new test

---
+++ src/components/__tests__/App.test.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "../App";
+
+it("shows a comment box", () => {
+  const div = document.createElement("div");
+
+  ReactDOM.render(<App />, div);
+
+  // Looks inside the div
+  // and checks if CommentBox is in there
+  expect(div.innerHTML).toContain("Comment Box");
+
+  // Cleanup
+  ReactDOM.unmountComponentAtNode(div);
+});
