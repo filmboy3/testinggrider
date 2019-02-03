26f1487358ca71e0ad4f9b70e35ecd408dfaaaff
filmboy3 <jonathanschwartz30@gmail.com>
2019-02-03 14:48:46 -0500

    added commentBox mount test

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,32 +1,5 @@
-import React, { Component } from "react";
+import React from "react";
 
-class CommentBox extends Component {
-  state = { comment: "" };
-
-  handleChange = event => {
-    this.setState({ comment: event.target.value });
-  };
-
-  handleSubmit = event => {
-    event.preventDefault();
-
-    // TODO - Call an action creator
-    // And save the comment
-
-    this.setState({ comment: "" });
-  };
-
-  render() {
-    return (
-      <form>
-        <h4>Add a Comment</h4>
-        <textarea onChange={this.handleChange} value={this.state.comment} />
-        <div>
-          <button onSubmit={this.handleSubmit}>Submit Comment</button>
-        </div>
-      </form>
-    );
-  }
-}
-
-export default CommentBox;
+export default () => {
+  return <div>Comment List</div>;
+};
